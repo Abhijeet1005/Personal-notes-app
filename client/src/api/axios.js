2c239cac7e2a29cb40080e3dd11ef1df8119cd27
Abhijeet1005 <aschauhan1052002@gmail.com>
2025-12-02 16:01:21 +0530

    feat: Create Axios API client with base URL and authentication token interceptor.

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.1.15:5000/api',
+    baseURL: 'https://personal-notes-app-1-akfa.onrender.com/api',
     headers: {
         'Content-Type': 'application/json'
     }
